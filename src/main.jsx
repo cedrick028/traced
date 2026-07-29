2ba537e60c48f0d9b8cba6654138a0f68d54e97e
cbayhon <cedrick.bayhon@corelab.com>
2026-07-29 09:31:11 +0400

    Fix auth context import casing

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { AuthProvider } from './context/authContext.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
 import { TransactionProvider } from './context/TransactionContext.jsx'
 import { BankProvider } from './context/bankContext.jsx'
 
